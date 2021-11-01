9a33f571c624da94c18f3ec5195b0acdc9fd248d
Hardik <hardikrathod712.hr@gmail.com>
2021-11-01 16:00:11 +0530

    Final

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
 const PORT = 8888;
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URL = process.env.REDIRECT_URL;
-const FRONTEND_URL = process.env.FRONTEND_URL;
+const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
+const CLIENT_SECRET = process.env.NEXT_PUBLIC_CLIENT_SECRET;
+const REDIRECT_URL = process.env.NEXT_PUBLIC_REDIRECT_URL;
+const FRONTEND_URL = process.env.NEXT_PUBLIC_FRONTEND_URL;
 const stateKey = 'SPOTIFY-AUTH-STATE';
 
 const express = require('express');
